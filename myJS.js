e07a7b7fb3458540177af3e17e23f9e5c21d242f
AyushTripathiIITP <101452500+AyushTripathiIITP@users.noreply.github.com>
2022-06-05 15:34:32 +0530

    added functional for reset button and less then zero

--- myJS.js
+++ myJS.js
@@ -1,9 +1,16 @@
 var decrement = document.getElementById("decrement");
 var increment = document.getElementById("increment");
 var counter = document.getElementById("counter");
+var reset = document.getElementById("reset");
 var count = 0;
 
 currentReading();
+
+reset.addEventListerner("click" , () =>
+{
+  count = 0;
+  currentReading();
+});
 
 decrement.addEventListener("click" , ()=> {
     --count;
